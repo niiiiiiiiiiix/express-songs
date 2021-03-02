b56e735d2f910f95bb86eea49bb86d6545997794
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-02 14:47:59 +0800

    Add GET /songs/:id test

--- __tests__/songs.test.js
+++ __tests__/songs.test.js
@@ -37,63 +37,53 @@ describe("App", () => {
     expect(response.body).toMatchObject(expectedSongsData);
   });
 
-  // it("GET /songs should be successful in returning the full song list", async () => {
-  //   const { body } = await request(app).get("/songs").expect(200);
+  it("POST /songs should be successful in adding item", async () => {
+    const newSong = {
+      name: "Pink Moon",
+      artist: "Nick Drake",
+    };
+    const expectedSong = {
+      name: "Pink Moon",
+      artist: "Nick Drake",
+    };
 
-  //   expect(body).toMatchObject([
-  //     {
-  //       name: "song 1",
-  //       artist: "artist 1",
-  //     },
-  //   ]);
+    const { body: actualSong } = await request(app)
+      .post("/songs")
+      .send(newSong)
+      .expect(201);
+    expect(actualSong).toMatchObject(expectedSong);
+  });
+
+  it("GET /songs/:id should return the correct song", async () => {
+    const song = await Song.findOne({ name: "song 1" });
+    const response = await request(app).get(`/songs/${song.id}`).expect(200);
+    expect(response.body.name).toEqual("song 1");
+
+    // const expectedSong = { name: "Pink Moon", artist: "Nick Drake" };
+    // const { body: actualSong } = await request(app).get("/songs/2").expect(200);
+    // we can actually just use "body" instead of "body:actualSong"
+    // body:actualSong is to rename the response to something that is more meaningful
+
+    // so if we use body then it can just be expect(body)....
+  });
+
+  // it("PUT /songs/:id should update the song entry", async () => {
+  //   const updatedSong = { name: "Pink Moon 2", artist: "Nick Drake 2" };
+  //   const expectedSong = {
+  //     id: 1,
+  //     name: "Pink Moon 2",
+  //     artist: "Nick Drake 2",
+  //   };
+  //   const { body } = await request(app)
+  //     .put("/songs/1")
+  //     .send(updatedSong)
+  //     .expect(200);
+  //   expect(body).toEqual(expectedSong);
+  // });
+
+  // it("DELETE /songs/:id should delete the correct song", async () => {
+  //   const deletedSong = { name: "Pink Moon 2", artist: "Nick Drake 2" };
+  //   const { body } = await request(app).delete("/songs/1").expect(200);
+  //   expect(body).toMatchObject(deletedSong);
   // });
 });
-
-//   it("POST /songs should be successful in adding item", async () => {
-//     const newSong = {
-//       name: "Pink Moon",
-//       artist: "Nick Drake",
-//     };
-//     const expectedSong = {
-//       id: 2,
-//       name: "Pink Moon",
-//       artist: "Nick Drake",
-//     };
-
-//     const { body: actualSong } = await request(app)
-//       .post("/songs")
-//       .send(newSong)
-//       .expect(201);
-//     expect(actualSong).toEqual(expectedSong);
-//   });
-
-//   it("GET /songs/:id should return the correct song", async () => {
-//     const expectedSong = { name: "Pink Moon", artist: "Nick Drake" };
-//     const { body: actualSong } = await request(app).get("/songs/2").expect(200);
-//     // we can actually just use "body" instead of "body:actualSong"
-//     // body:actualSong is to rename the response to something that is more meaningful
-
-//     expect(actualSong).toMatchObject(expectedSong);
-//     // so if we use body then it can just be expect(body)....
-//   });
-
-//   it("PUT /songs/:id should update the song entry", async () => {
-//     const updatedSong = { name: "Pink Moon 2", artist: "Nick Drake 2" };
-//     const expectedSong = {
-//       id: 1,
-//       name: "Pink Moon 2",
-//       artist: "Nick Drake 2",
-//     };
-//     const { body } = await request(app)
-//       .put("/songs/1")
-//       .send(updatedSong)
-//       .expect(200);
-//     expect(body).toEqual(expectedSong);
-//   });
-
-//   it("DELETE /songs/:id should delete the correct song", async () => {
-//     const deletedSong = { name: "Pink Moon 2", artist: "Nick Drake 2" };
-//     const { body } = await request(app).delete("/songs/1").expect(200);
-//     expect(body).toMatchObject(deletedSong);
-//   });
-// });
