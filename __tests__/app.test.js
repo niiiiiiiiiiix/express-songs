2294e7d24b252ad6294c5ab50ac886cba89842da
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-02 14:21:12 +0800

    Re-enabled app and movie tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,35 +5,33 @@ describe("App", () => {
   it("should pass", () => {
     expect(1).toBe(1);
   });
+
+  it("GET / should respond with Hello World", async () => {
+    const response = await request(app).get("/");
+    // const response = await request(app).get("/").expect(200); then can remove expect...toBe(200)
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello World");
+  });
+
+  it("GET / should respond with Hello World", async () => {
+    const { text, status } = await request(app).get("/");
+
+    expect(status).toBe(200);
+    expect(text).toBe("Hello World");
+  });
+
+  it("POST / should throw error when sending non-json content", async () => {
+    const { text } = await request(app).post("/").send("non-json").expect(400);
+    expect(text).toBe("Not json");
+  });
+
+  it("POST / should be successful when sending json content", async () => {
+    const { text } = await request(app)
+      .post("/")
+      .send({ name: "nix" })
+      .expect(201);
+
+    expect(text).toBe("Thanks for the JSON!");
+  });
 });
-
-// describe.skip("App", () => {
-//   it("GET / should respond with Hello World", async () => {
-//     const response = await request(app).get("/");
-//     // const response = await request(app).get("/").expect(200); then can remove expect...toBe(200)
-
-//     expect(response.status).toBe(200);
-//     expect(response.text).toBe("Hello World");
-//   });
-
-//   it("GET / should respond with Hello World", async () => {
-//     const { text, status } = await request(app).get("/");
-
-//     expect(status).toBe(200);
-//     expect(text).toBe("Hello World");
-//   });
-
-//   it("POST / should throw error when sending non-json content", async () => {
-//     const { text } = await request(app).post("/").send("non-json").expect(400);
-//     expect(text).toBe("Not json");
-//   });
-
-//   it("POST / should be successful when sending json content", async () => {
-//     const { text } = await request(app)
-//       .post("/")
-//       .send({ name: "nix" })
-//       .expect(201);
-
-//     expect(text).toBe("Thanks for the JSON!");
-//   });
-// });
--- __tests__/movie.test.js
+++ __tests__/movie.test.js
@@ -1,53 +1,44 @@
 const request = require("supertest");
 const app = require("../app");
 
-describe("App", () => {
-  it("should pass", () => {
-    expect(1).toBe(1);
+describe("movies", () => {
+  it("POST /movies should return a new movie object", async () => {
+    const newMovie = { movieName: "Lion King" };
+    const { body } = await request(app)
+      .post("/movies")
+      .send(newMovie)
+      .expect(201);
+    expect(body).toMatchObject(newMovie);
+  });
+
+  it("GET /movies should return an array containing one movie", async () => {
+    const { body } = await request(app).get("/movies").expect(200);
+    expect(body).toMatchObject([{ movieName: "Lion King" }]);
+  });
+
+  it("GET /movies/:movieID should return the movie with id", async () => {
+    const expectedMovie = { movieName: "Lion King" };
+    const { body } = await request(app).get("/movies/1").expect(200);
+    expect(body).toMatchObject(expectedMovie);
+  });
+
+  it("PUT /movies/:movieID should return the movie with id", async () => {
+    const updatedMovie = { movieName: "Frozen 2" };
+    const { body } = await request(app)
+      .put("/movies/1")
+      .send(updatedMovie)
+      .expect(200);
+    expect(body).toMatchObject(updatedMovie);
+  });
+
+  it("DELETE /movies/:movieID should return the deleted movie", async () => {
+    const deletedMovie = { movieName: "Frozen 2" };
+    const { body } = await request(app).delete("/movies/1").expect(200);
+    expect(body).toMatchObject(deletedMovie);
+  });
+
+  it("GET /movies should return an empty array", async () => {
+    const { body } = await request(app).get("/movies").expect(200);
+    expect(body).toMatchObject([]);
   });
 });
-
-// const request = require("supertest");
-// const app = require("../app");
-
-// describe.skip("movies", () => {
-//   it("POST /movies should return a new movie object", async () => {
-//     const newMovie = { movieName: "Lion King" };
-//     const { body } = await request(app)
-//       .post("/movies")
-//       .send(newMovie)
-//       .expect(201);
-//     expect(body).toMatchObject(newMovie);
-//   });
-
-//   it("GET /movies should return an array containing one movie", async () => {
-//     const { body } = await request(app).get("/movies").expect(200);
-//     expect(body).toMatchObject([{ movieName: "Lion King" }]);
-//   });
-
-//   it("GET /movies/:movieID should return the movie with id", async () => {
-//     const expectedMovie = { movieName: "Lion King" };
-//     const { body } = await request(app).get("/movies/1").expect(200);
-//     expect(body).toMatchObject(expectedMovie);
-//   });
-
-//   it("PUT /movies/:movieID should return the movie with id", async () => {
-//     const updatedMovie = { movieName: "Frozen 2" };
-//     const { body } = await request(app)
-//       .put("/movies/1")
-//       .send(updatedMovie)
-//       .expect(200);
-//     expect(body).toMatchObject(updatedMovie);
-//   });
-
-//   it("DELETE /movies/:movieID should return the deleted movie", async () => {
-//     const deletedMovie = { movieName: "Frozen 2" };
-//     const { body } = await request(app).delete("/movies/1").expect(200);
-//     expect(body).toMatchObject(deletedMovie);
-//   });
-
-//   it("GET /movies should return an empty array", async () => {
-//     const { body } = await request(app).get("/movies").expect(200);
-//     expect(body).toMatchObject([]);
-//   });
-// });
